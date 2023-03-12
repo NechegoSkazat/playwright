ebc1274a808457b6631c6070443ffca004f81c2d
Dmitriy Sergeev <d.sergeev@nuzhnapomosh.ru>
2023-03-12 12:48:12 +0000

    Update login-main-np.spec.js

--- login-main-np.spec.js
+++ login-main-np.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+// логин с главной нп по паролю и возврат после логина снова на главную нп
 test('test', async ({ page }) => {
   await page.goto('https://nuzhnapomosh.ru/');
   await page.getByRole('link', { name: 'войти' }).click();
